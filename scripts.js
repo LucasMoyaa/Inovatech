1f8ae9c565fcb69e35a19732117ca77cd897aa70
João Pedro <joaopedropalazzolli@gmail.com>
2024-04-16 21:15:13 -0300

    Criado conexão com a api, ajustes no css e no html

--- scripts.js
+++ scripts.js
@@ -11,60 +11,94 @@ document.addEventListener('DOMContentLoaded', function() {
     const smokeValueElement = document.querySelector('#smoke-value')
     const ctx = document.getElementById('myChart').getContext('2d');
 
-    let chartData = {
-        labels: ['0h', '1h', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', '11h', '12h', '13h', '14h', '15h', '16h', '17h', '18h', '19h', '20h', '21h', '22h', '23h'],
-        datasets: [{
-            label: 'Dados de Qualidade do Ar',
-            data: [10, 20, 15, 25, 30, 28, 22, 18, 16, 20, 25, 27, 30, 28, 26, 24, 22, 20, 18, 15, 12, 10, 8, 5],
-            borderColor: 'blue',
-            backgroundColor: 'rgba(0, 0, 255, 0.2)',
-        }]
-    };
+    const serverURL = 'http://localhost:8080/api/v1/monitoramento';
 
-    let myChart = new Chart(ctx, {
-        type: 'line',
-        data: chartData,
-        options: {
-            responsive: false, // Desativar a responsividade
-            maintainAspectRatio: false,
-            aspectRatio: 2, // Proporção personalizada
-        }
-    });
+    let monitoramentoData = [];
 
-    function updateSensorData() {
-        let smoke = "Não Detectada";
-        tempValue = Math.floor(Math.random() * 50);
-        thermometerHeightPercentage = (tempValue / 50) * 100;
-        humidityValue = Math.floor(Math.random() * 100);
-        smokeValue = Math.floor(Math.random() * 50);
+    fetch(serverURL,
+        {
+            headers: {
+                "Accept": "*/*",
+                "Content-Type": "application/json",
+            },
+            method: "GET"
+        })
+        .then(function (res) {
+            if(res.status >= 200 && res.status <= 299){
+                console.log(res)
+                res.json().then((produtos) => {
 
-        if(smokeValue > 10){
-            smoke = "Detectada"
-        }
+                    produtos.map(p => {
+                        monitoramentoData.push(p);
+                    })
+                
+                    let chartData = {
+                            labels: ['0h', '1h', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', '11h', '12h', '13h', '14h', '15h', '16h', '17h', '18h', '19h', '20h', '21h', '22h', '23h'],
+                            datasets: [{
+                            label: 'Dados de Qualidade do Ar',
+                            data: monitoramentoData.map(x => x.temperatura),
+                            borderColor: 'blue',
+                            backgroundColor: 'rgba(0, 0, 255, 0.2)',
+                        }]
+                    };
+                    
+                    
 
-        smokeValueElement.textContent = `${smoke}`;
-        temperatureValueElement.textContent = `${tempValue}°C`;
-        humidityValueElement.textContent = `${humidityValue}%`;
-        humidityFillElement.style.width = `${humidityValue}%`;
-        thermometerFillElement.style.width = `${thermometerHeightPercentage}%`;
+                    let myChart = new Chart(ctx, {
+                        type: 'line',
+                        data: chartData,
+                        options: {
+                            responsive: false, // Desativar a responsividade
+                            maintainAspectRatio: false,
+                            aspectRatio: 2, // Proporção personalizada
+                        }
+                    });
 
-        if (tempValue >= 50) {
-            document.body.classList.add('temperature-alert');
-            alertSound.play();
-        } else {
-            document.body.classList.remove('temperature-alert');
-            alertSound.pause();
-        }
+                    function updateSensorData() {
+                        let smoke = "Não Detectada";
+                        tempValue = Math.floor(Math.random() * 50);
+                        thermometerHeightPercentage = (tempValue / 50) * 100;
+                        humidityValue = Math.floor(Math.random() * 100);
+                        smokeValue = Math.floor(Math.random() * 50);
+                
+                        if(smokeValue == true){
+                            smoke = "Detectada"
+                        }
+                
+                        smokeValueElement.textContent = `${smoke}`;
+                        temperatureValueElement.textContent = `${tempValue}°C`;
+                        humidityValueElement.textContent = `${humidityValue}%`;
+                        humidityFillElement.style.width = `${humidityValue}%`;
+                        thermometerFillElement.style.width = `${thermometerHeightPercentage}%`;
+                
+                        if (tempValue >= 50) {
+                            document.body.classList.add('temperature-alert');
+                            alertSound.play();
+                        } else {
+                            document.body.classList.remove('temperature-alert');
+                            alertSound.pause();
+                        }
+                
+                        // Atualize os dados do gráfico a cada hora
+                        
+                        chartData.datasets[0].data.push(tempValue); // verificar id
+                        if(chartData.datasets[0].data.length == 25){
+                            chartData.datasets[0].data = [];
+                        }
+                        
+                        myChart.update();
+                    }
+                    
+                    updateSensorData();
+                    // Chame a função de atualização a cada hora (3600000 milissegundos)
+                    setInterval(function(){
+                        updateSensorData();
+                    }, 6000); // 3600000 tentar sincronizar com hora da api
+                })
 
-        // Atualize os dados do gráfico a cada hora
-        chartData.datasets[0].data.push(tempValue);
-        chartData.datasets[0].data.shift();
-        myChart.update();
-    }
+            }})
+            
+        .catch(function (res) { console.log(res) })
+
     
-    updateSensorData();
-    // Chame a função de atualização a cada hora (3600000 milissegundos)
-    setInterval(function(){
-        updateSensorData();
-    }, 60000); // 3600000
 });
